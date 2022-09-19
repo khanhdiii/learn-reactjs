82001795910f0e37f581e8731df3c62d3e591b14
khanhdiii <khanh12369874@gmail.com>
2022-09-19 15:46:54 +0700

    Updates

---
+++ precache-manifest.15426de7f188064d537836d86e12569e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cc5cc88dd15ec255287a6c158aab38",
+    "url": "/learn-reactjs/index.html"
+  },
+  {
+    "revision": "4fbaca5f007e934c7e18",
+    "url": "/learn-reactjs/static/css/main.bf193d70.chunk.css"
+  },
+  {
+    "revision": "900612d7bad8dbc8e2fd",
+    "url": "/learn-reactjs/static/js/2.15960923.chunk.js"
+  },
+  {
+    "revision": "ee537950629bf98007e99cf7c361c592",
+    "url": "/learn-reactjs/static/js/2.15960923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbaca5f007e934c7e18",
+    "url": "/learn-reactjs/static/js/main.66e8af02.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/learn-reactjs/static/js/main.66e8af02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ee37ebb763f80062d1",
+    "url": "/learn-reactjs/static/js/runtime-main.ecebdde8.js"
+  }
+]);
