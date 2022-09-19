314936643b107cc878372ae3a95d73b2ffe46d32
khanhdiii <khanh12369874@gmail.com>
2022-09-19 16:22:45 +0700

    add plugin chat mess

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Header from 'components/Header';
 import ProductFeature from 'features/product';
 import TodoFeature from 'features/Todo/pages/ListPage';
@@ -7,6 +6,7 @@ import NotFound from './components/NotFound';
 import AlbumFeature from './features/Album';
 import CartFeature from './features/Cart';
 import CounterFeature from './features/Counter';
+import './script';
 import React from 'react';
 
 function App() {
@@ -16,12 +16,16 @@ function App() {
       <Switch>
         <Route path="/" component={CounterFeature} exact />
         <Route path="/todos" component={TodoFeature} />
-        <Route path="/albums" component={AlbumFeature}/>
-        <Route path="/products" component={ProductFeature}/>
-        <Route path="/cart" component={CartFeature}/>
+        <Route path="/albums" component={AlbumFeature} />
+        <Route path="/products" component={ProductFeature} />
+        <Route path="/cart" component={CartFeature} />
 
-        <Route component={NotFound}/>
+        <Route component={NotFound} />
       </Switch>
+      <div id="fb-root"></div>
+
+      {/* <!-- Your Plugin chat code --> */}
+      <div id="fb-customer-chat" class="fb-customerchat"></div>
     </div>
   );
 }
