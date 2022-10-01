931fa011709185c4a1dd825a68f5083972f6f089
khanhdiii <khanh12369874@gmail.com>
2022-10-01 11:55:48 +0700

    update sass package

--- src/features/Album/components/AlbumList/index.jsx
+++ src/features/Album/components/AlbumList/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Album from '../Album';
-import './styles.scss'
+// import './styles.scss'
 AlbumList.propTypes = {
     albumList: PropTypes.array.isRequired,
 };
--- src/features/product/components/ProductInfo.jsx
+++ src/features/product/components/ProductInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, makeStyles, Typography } from '@material-ui/core';
-import '../components/scss/ProductInfo.scss';
+// import '../components/scss/ProductInfo.scss';
 import { formatPrice } from 'utils';
 ProductInfo.propTypes = {};
 
